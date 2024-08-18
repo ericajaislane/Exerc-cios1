af302094e86816d780f2a49969efbcd0464dae7b
Erica Jaislane <152648635+ericajaislane@users.noreply.github.com>
2024-08-17 23:00:37 -0300

    exercicio1
    Resolvida

---
+++ Sem Título1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int valor;
+    printf("Digite um valor: ");
+    scanf("%d", &valor);
+    printf("O antecessor de %d é %d\n", valor, valor - 1);
+    return 0;
+}
+
+
+
+
