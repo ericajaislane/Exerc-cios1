2a016b721437cc2f06442c7776c85e6d6a5ff8c8
Erica Jaislane <152648635+ericajaislane@users.noreply.github.com>
2024-08-21 13:10:45 -0300

    Update Sem Título6.c

--- Sem Título6.c
+++ Sem Título6.c
@@ -2,10 +2,22 @@
 
 int main() {
     float custoFabrica, custoFinal;
-    printf("Digite o custo de f�brica do carro: ");
+    printf("Digite o custo de fábrica do carro: ");
     scanf("%f", &custoFabrica);
     custoFinal = custoFabrica + (custoFabrica * 0.28) + (custoFabrica * 0.45);
-    printf("O custo final ao consumidor �: %.2f\n", custoFinal);
+    printf("O custo final ao consumidor é: %.2f\n", custoFinal);
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    float custoFabrica, custoFinal;
+    printf("Digite o custo de fábrica do carro: ");
+    scanf("%f", &custoFabrica);
+    custoFinal = custoFabrica + (custoFabrica * 0.28) + (custoFabrica * 0.45);
+    printf("O custo final ao consumidor é: %.2f\n", custoFinal);
+    return 0;
+}
+
 
